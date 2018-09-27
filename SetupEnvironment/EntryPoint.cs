0d8687c59cbe74ecbfd13eb2f6f3ce1dbf68cd48
Railson Rames <rames.cloud@outlook.com.br>
2018-09-27 11:28:22 -0300

    Revert "Capturando elemento da tela pela propriedade "name" e tratando asserts de maneira inicial, alguns tratamentos seriam interessantes como o uso do bloco "try catche", mas o objetivo foi atingido com o retorno em console com a condição lógica."
    This reverts commit aaa53f0ea9b60ac8dca43a16ea80f8431b66cb47.

--- SetupEnvironment/EntryPoint.cs
+++ SetupEnvironment/EntryPoint.cs
@@ -1,6 +1,6 @@
-using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 class EntryPoint
 {
@@ -8,33 +8,10 @@ class EntryPoint
     {
         IWebDriver driver = new ChromeDriver();
 
-        driver.Navigate().GoToUrl("http://testing.todvachev.com/selectors/name/");
+        driver.Navigate().GoToUrl("http://testing.todvachev.com/");
 
-        IWebElement element = driver.FindElement(By.Name("myName"));
-
-        if (element.Displayed)
-        {
-            MensagemVerde("Sucesso, eu posso ver o elemento!");
-        }
-        else
-        {
-            MensagemVermelha("Falhou, alguma bicheira aconteceu!");
-        }
+        Thread.Sleep(3000);
 
         driver.Quit();
-    }
-
-    private static void MensagemVermelha(string mensagem)
-    {
-        
-        Console.BackgroundColor = ConsoleColor.Red;
-        Console.WriteLine(mensagem);     
-        Console.BackgroundColor = backupDeCor;
-    }
-
-    private static void MensagemVerde(string mensagem)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(mensagem);
     }
 }
